01da461555ef7016f8619d29a64cb0bceca0fbc8
Vany Liu <vany8462@gmail.com>
2020-04-30 21:49:38 +0800

    add chat3.py

---
+++ chat3.py
@@ -0,0 +1,13 @@
+
+lines = []
+with open('3.txt', 'r', encoding='utf-8') as f:
+	for line in f:
+		lines.append(line.strip())
+ 
+for line in lines:
+	s = line.split(' ')
+	time = s[0][:5]
+	name = s[0][5:]
+	print(s)
+	print(time)
+	print(name)
